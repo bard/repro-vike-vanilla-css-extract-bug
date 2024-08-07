2ed9e7a147bcf5bb5504cb6402fd3b276a084af6
Massimiliano Mirra <bard@hyperstruct.net>
2024-08-07 09:14:12 +0200

    repro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
-  plugins: [vike({}), react({})],
+  plugins: [vike({ prerender: true }), react({}), vanillaExtractPlugin()],
 });
